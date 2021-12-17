cbe9b5030cac0a8ce445b01b0544000ea5baf4bc
daxtonparke <daxtonparke@gmail.com>
2021-12-17 12:32:44 -0700

    out of time

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -18,3 +18,15 @@ test('Title shows up when page loads', async () => {
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
+
+test('should check that clicking draw button displays choices', async()=>{
+    await driver.findElement(By.id('draw')).click()
+    let robotDiv = driver.findElement(By.id('choices'))
+    const displayed = await robotDiv.isDisplayed()
+    expect(displayed).toBe(true)
+})
+
+test('add to duo displays player id', async()=>{
+    await driver.findElement(By.id('draw')).click()
+    await driver.findElement(By.id('')).click()
+})
